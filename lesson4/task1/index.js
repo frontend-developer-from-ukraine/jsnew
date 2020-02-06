59c3d09677c68b637dd6e10437bd3bfffeb2c94f
Ivan Borshchevskyi <58000246+frontend-developer-from-ukraine@users.noreply.github.com>
2020-02-06 06:37:49 -0800

    add

--- lesson4/task1/index.js
+++ lesson4/task1/index.js
@@ -1,8 +1,10 @@
-const n = 120;
-let sum = 0;
+const m = 100;
+const n = 5;
 
-for (let i = 0; i <= n; i++) {
-    sum += i;
+let result = 1;
+
+for (let i = m; i <= n; i++) {
+    if (i % 2 === 1) {
+        result *= i;
+    }
 }
-
-console.log(sum);
